01e77eed304671ef57d452ca1898f70ff72e8693
MINDHELL <Tgpurpose640@gmail.com>
2025-01-13 20:59:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,20 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    API_ID = environ.get("API_ID", "27788368")
+    API_HASH = environ.get("API_HASH", "9df7e9ef3d7e4145270045e5e43e1081")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7872064052:AAEMj0pWHSV7z2xe5EVB_FWvzs9oDh4JKOI") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6860316927').split()]
+    BOT_SESSION = environ.get("BOT_SESSION", "Forward090bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
     
     DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "")
     
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")  # FORCE SUB ON - OFF
 
 
 class temp(object): 
