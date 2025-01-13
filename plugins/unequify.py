c8d823da754e25e67287217be8fa1f6a705f65e8
MINDHELL <Tgpurpose640@gmail.com>
2025-01-13 20:05:01 +0530

    Update unequify.py

--- plugins/unequify.py
+++ plugins/unequify.py
@@ -9,8 +9,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 CLIENT = CLIENT()
 COMPLETED_BTN = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton('⚡ Support', url='https://t.me/Jisshu_support')],
-        [InlineKeyboardButton('📢 Updates', url='https://t.me/JISSHU_BOTS')]
+        [InlineKeyboardButton('⚡ Support', url='https://t.me/Xsupport_chats)],
+        [InlineKeyboardButton('📢 Updates', url='https://t.me/allvidsbackup1)]
     ]
 )
 
